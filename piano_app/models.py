4584c083216d0ffd2ac6d7427398bcd107d5e1b1
Avi Schwartz <Schwartz210@gmail.com>
2017-11-13 14:46:25 -0500

    Update models.py

--- piano_app/models.py
+++ piano_app/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from piano_app.models import UserProfileInfo
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username','email','password')
 
 # Create your models here.
 class UserProfileInfoForm(models.Model):
