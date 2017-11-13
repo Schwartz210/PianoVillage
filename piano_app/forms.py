6a9c471f1fcbc5967d2006c75140fff670cc4157
Avi Schwartz <Schwartz210@gmail.com>
2017-11-13 14:09:06 -0500

    Create forms.py

---
+++ piano_app/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.models import User
+from piano_app.models import UserProfileInfo
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username','email','password')
+
+
+class UserProfileInfoForm(forms.ModelForm):
+    class Meta():
+        model = UserProfileInfo
+        fields = ('date_of_birth',
+                  'sex',
+                  'school_name',
+                  'grade',
+                  'address1',
+                  'address2',
+                  'city',
+                  'state',
+                  'zip_code',
+                  'phone',
+                  'email',
+                  'volunteer_experience',
+                  'interest_in_piano_village',
+                  'goals_for_service_to_the_community',
+                  'emergency_name',
+                  'emergency_phone',
+                  'relationship',
+                  'how_did_you_hear_about_us')
