4f59b005aab6a3514cdde8a588aac43dbfe0299c
Avi Schwartz <Schwartz210@gmail.com>
2017-11-13 14:32:17 -0500

    Update views.py

--- piano_app/views.py
+++ piano_app/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
-from django.conf.urls import url, include
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
+def special(request):
+    # Remember to also set login url in settings.py!
+    # LOGIN_URL = '/basic_app/user_login/'
+    return HttpResponse("You are logged in. Nice!")
 
+@login_required
+def user_logout(request):
+    # Log out the user.
+    logout(request)
+    # Return to homepage.
+    return HttpResponseRedirect(reverse('index'))
 
 def about(request):
     return render(request, 'piano_app/about.html')
@@ -29,7 +42,7 @@ def index(request):
     return render(request, 'piano_app/index.html')
 
 def members(request):
-    return render(request, 'piano_app/v.html')
+    return render(request, 'piano_app/members.html')
 
 def news(request):
     return render(request, 'piano_app/news.html')
@@ -50,7 +63,28 @@ def program(request):
     return render(request, 'piano_app/program.html')
 
 def signup(request):
-    return render(request, 'piano_app/signup.html')
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors,profile_form.errors)
+    else:
+        # Was not an HTTP post so we just render the forms as blank.
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+    return render(request,'piano_app/signup.html',
+                          {'user_form':user_form,
+                           'profile_form':profile_form,
+                           'registered':registered})
 
 def sponsors(request):
     return render(request, 'piano_app/sponsors.html')
